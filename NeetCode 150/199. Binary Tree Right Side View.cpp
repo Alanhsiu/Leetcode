bbcd2cae5357c684080ef2822cd019a6601c3b92
Alanhsiu <alan901101@gmail.com>
2026-03-15 00:10:53 -0700

    199. Binary Tree Right Side View.cpp

---
+++ NeetCode 150/199. Binary Tree Right Side View.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& res, int level){
+        if(!root){
+            return;
+        }
+        if(level==res.size()){
+            res.push_back(root->val);
+        }
+        dfs(root->right, res, level+1);
+        dfs(root->left, res, level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        dfs(root, res, 0);
+        return res;
+    }
+};
+
+// Time Complexity: O(N)
+// Space Complexity: O(H), where H is the height of the binary tree, which is O(N) in the worst case (skewed tree) and O(log N) in the best case (balanced tree).

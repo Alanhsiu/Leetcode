46070d12f71d99910309d515b1aa70631c296079
Alanhsiu <alan901101@gmail.com>
2026-06-11 17:13:10 -0700

    252. Meeting Rooms.cpp

---
+++ NeetCode 150/252. Meeting Rooms.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        for(int i=1; i<intervals.size(); ++i){
+            if(intervals[i][0]<intervals[i-1][1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Time: O(nlogn)
+// Space: O(1)
